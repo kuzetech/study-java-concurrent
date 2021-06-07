357ce97f54878e2b0ca7b32bd4ee4e7de3716371
黄少威 <huangshaowei@xmfunny.com>
2021-06-07 19:48:45 +0800

    增加并发容器文档

---
+++ src/main/java/com/kuze/bigdata/study/l5collection/CollectionDoc.java
@@ -0,0 +1,38 @@
+package com.kuze.bigdata.study.l5collection;
+
+public class CollectionDoc {
+
+    /**
+     *
+     * List
+     * CopyOnWriteArrayList
+     *
+     * Map
+     * ConcurrentHashMap
+     * ConcurrentSkipListMap 有序key
+     *
+     * Set
+     * CopyOnWriteArraySet
+     * ConcurrentSkipListSet
+     *
+     * BlockingQueue
+     * ArrayBlockingQueue   有界
+     * LinkedBlockingQueue  有界
+     * SynchronousQueue
+     * LinkedTransferQueue 结合 LinkedBlockingQueue 和 SynchronousQueue 功能
+     * PriorityBlockingQueue 支持优先级出队
+     * DelayQueue 支持延迟出队
+     *
+     * BlockingDeque
+     * LinkedBlockingDeque
+     *
+     * NoBlockingQueue
+     * ConcurrentLinkedQueue
+     *
+     * NoBlockingDeque
+     * ConcurrentLinkedDeque
+     *
+     *
+     */
+
+}
